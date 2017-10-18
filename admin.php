42a39d6b6480dceb7ad29cbac2ee288f04f9579d
Gustavo Lessa <gustavolessadublin@gmail.com>
2017-10-18 09:04:24 +0100

    file structure, login and register from/to DB
    file structure, login and register from/to DB

---
+++ admin.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+	// get session variables
+
+	if($_SESSION["pageOrigin"] == "register"){
+		if (isset($_SESSION["username"]))  {
+			echo "<br/>Registered as: ".$_SESSION["username"];
+		}
+	}
+
+	if($_SESSION["pageOrigin"] == "login"){
+		if (isset($_SESSION["username"]))  {
+			echo "<br/>Logged in as: ".$_SESSION["username"];
+		}
+	}
+
+	$row = $_SESSION["resultsRow"];
+		if (!empty($row)){ //is the array empty
+			$username = $row['username'];
+		} else {
+		    $message= 'Sorry your log in details are not correct';
+		}
+
+		echo '<br><a href="logout.php?token='.md5(session_id()).'">Logout</a>';
+
+
+?>
+
+</body>
+</html>
