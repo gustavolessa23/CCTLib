cb66f103846567aaa8cc832d3435118afa936bd5
Gustavo Lessa <gustavolessadublin@gmail.com>
2017-12-11 12:40:27 +0000

    Error handling improvements.

--- errordb.php
+++ errordb.php
@@ -2,7 +2,7 @@
 try{
   $arr = $stmt->errorInfo();
 } catch(PDOException $e) {
-  echo "PDO error :" . $exception->getMessage();
+  echo "PDO error :" . $e->getMessage();
 }
 if (isset($arr[2])) {// we have an error
  echo "<br/> Database Error Code: ".$arr[0];
